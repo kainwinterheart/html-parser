1fc6c29ed1e612d93f311aba0a23767e0dd1282e
Gennadiy Filatov <gennadiy@yandex-team.ru>
2014-10-04 02:29:28 +0400

    XS draft #3

--- t.pl
+++ t.pl
@@ -6,7 +6,10 @@ use warnings;
 use lib 'blib/lib';
 use blib 'blib/arch/auto/';
 
-use Salvation::HTMLLike::XS;
+use Salvation::HTMLLike::XS ();
+use Data::Dumper 'Dumper';
+
+print Dumper( Salvation::HTMLLike::XS::tokenize( shift( @ARGV ) ) );
 
 exit 0;
 
